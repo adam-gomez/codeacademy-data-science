7530798bd643f174b7f01a1043d912d585bf9ff5
Adam Gomez <adam.daniel.gomez.787@gmail.com>
2020-10-24 20:43:30 -0500

    Completed mini-project

---
+++ python_lists_medical_insurance_project.py
@@ -0,0 +1,43 @@
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.", '\n')
+  return estimated_cost
+ 
+# Estimate Maria's insurance cost
+maria_insurance_cost = estimate_insurance_cost(name = "Maria", age = 31, sex = 0, bmi = 23.1, num_of_children = 1, smoker = 0)
+
+# Estimate Rohan's insurance cost
+rohan_insurance_cost = estimate_insurance_cost(name = 
+"Rohan", age = 25, sex = 1, bmi = 28.5, num_of_children = 3, smoker = 0)
+
+# Estimate Valentina's insurance cost
+valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
+
+# Add your code here
+
+names = ['Maria', 'Rohan', 'Valentina']
+insurance_costs = [4150.0, 5320.0, 35210.0]
+
+#Estimate Akira's insurance cost
+akira_insurance_cost = estimate_insurance_cost(name = "Akira", age = 19, sex = 1, bmi = 27.1, num_of_children = 0, smoker = 0)
+
+
+
+insurance_data = list(zip(names, insurance_costs))
+insurance_data.append(('Akira', 2930.0))
+print(f'Here is the actual insurance cost data: {insurance_data}', '\n')
+
+estimated_insurance_data = []
+estimated_insurance_data.append(('Maria', maria_insurance_cost))
+estimated_insurance_data.append(('Rohan', rohan_insurance_cost))
+estimated_insurance_data.append(('Valentina', valentina_insurance_cost))
+estimated_insurance_data.append(('Akira', akira_insurance_cost))
+
+print(f'Here is the estimated insurance cost data: {estimated_insurance_data}', '\n')
+
+insurance_cost_difference = []
+for i in range(len(estimated_insurance_data)):
+  insurance_cost_difference.append((estimated_insurance_data[i][0], estimated_insurance_data[i][1] - insurance_data[i][1]))
+
+print(f'The differences between the estimates and the actual costs (estimate - actual): {insurance_cost_difference}')
